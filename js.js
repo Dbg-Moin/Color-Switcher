ac98028d7c6bda1043783e15466fcda012f8f90d
Dbg-Moin <Dbg-Moin@users.github.com>
2021-01-12 00:32:42 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,7 @@
+function changeBodyBg(color){
+    document.body.style.background = color;
+}
+
+function changeHeadingBg(color){
+    document.getElementById("heading").style.background = color;
+}
